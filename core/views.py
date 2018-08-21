a9b70fcb98b6fc2f60dcf8734c2056a3d81cefcd
Sergio Gabriel <sgrodriguez@gmail.com>
2018-08-21 10:21:03 -0300

    Agregado de template tags para incorporar el menú en las plantillas

--- core/views.py
+++ core/views.py
@@ -1,28 +1,18 @@
 from django.shortcuts import render, HttpResponse
 
 
-html_base = """
-<h1>Web Personal</h1>
-<ul>
-    <li><a href="/">Portada</a></li>
-    <li><a href="/about-me">Acerca de</a></li>
-    <li><a href="/portfolio">Portafolio</a></li>
-    <li><a href="/contact">Contacto</a></li>
-</ul>
-"""
-
 # Create your views here.
 def home(request):
-    return HttpResponse(html_base + "<h2>Portada</h2><p>Bienvenidos!</p>")
+    return render(request, "core/home.html")
 
 
 def about(request):
-    return HttpResponse(html_base + "<h2>Acerca de...</h2><p>Mi nombre es SGR</p>")
+    return render(request, "core/about.html")
 
 
 def portfolio(request):
-    return HttpResponse(html_base + "<h2>Portafolio</h2><p>Proyectos desarrollados</p>")
+    return render(request, "core/portfolio.html")
 
 
 def contact(request):
-    return HttpResponse(html_base + "<h2>Contacto</h2><p>Comunicarse al 800-111-222</p>")
+    return render(request, "core/contact.html")
