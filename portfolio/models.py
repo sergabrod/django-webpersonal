bd81b43582031f6d3e14c6c6d285c4bc1142d995
Sergio Gabriel <sgrodriguez@gmail.com>
2018-08-24 12:42:38 -0300

    Creación de modelos y admin

--- portfolio/models.py
+++ portfolio/models.py
@@ -9,3 +9,8 @@ class Project(models.Model):
     # auto_now_add sólo se guarda al crearse
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "proyecto"
+        verbose_name_plural = "proyectos"
+        ordering = ["-created"]
