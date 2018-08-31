0946c8c4ea4861167cd2e759af382550e2b3297b
Sergio Gabriel <sgrodriguez@gmail.com>
2018-08-31 11:47:04 -0300

    Modificación de Admin Django

--- portfolio/models.py
+++ portfolio/models.py
@@ -3,14 +3,18 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    image = models.ImageField()
+    title = models.CharField(max_length=200, verbose_name='Título')
+    description = models.TextField(verbose_name='Descripción')
+    image = models.ImageField(verbose_name='Imágen', upload_to='projects')
     # auto_now_add sólo se guarda al crearse
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de creación')
+    updated = models.DateTimeField(auto_now=True, verbose_name='Fecha de edición')
 
     class Meta:
         verbose_name = "proyecto"
         verbose_name_plural = "proyectos"
         ordering = ["-created"]
+
+    def __str__(self):
+        return self.title
+
